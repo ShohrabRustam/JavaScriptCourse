fe9ca4a72b8487bc4beca25770a2591a5ef4e5ac
Shohrab Rustam <mohdrustam001@gmail.com>
2022-03-02 14:48:49 +0530

    parameterised prototype in js

--- FirstDay/objectPrototype.js
+++ FirstDay/objectPrototype.js
@@ -7,3 +7,12 @@ var abc = {
 
 document.write(abc);
 console.log(abc)
+
+var xyz = function(Name, Subject , Address ){
+    this.Name=Name;
+    this.Subject=Subject;
+    this.Address=Address;
+}
+
+let x = new xyz("Rustam", "Problem Solving","New Delhi");
+console.log(x);
