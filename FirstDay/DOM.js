2986b7261ae2cd738ea6bc63df1a9bfe7cd539ff
Shohrab Rustam <mohdrustam001@gmail.com>
2022-03-02 19:23:50 +0530

    get element by class name and get element by Tag Name

--- FirstDay/DOM.js
+++ FirstDay/DOM.js
@@ -1,5 +1,7 @@
 function TaskFun(){
 var x = document.getElementsByTagName('div');
+
+// arr= array("red","green","blue","Black");
 /*x[0].style.color="Red";
 x[1].style.color="blue";
 x[2].style.color="yellow";
@@ -12,5 +14,16 @@ for(let i=0;i<x.length;i++){
     x[i].style.border="2px solid black";
     x[i].style.margin="5px";
 }
+var y = document.getElementsByClassName('demo');
+
+for(let i=0;i<y.length;i++){
+    y[i].style.fontWeight="Bolder";
+    y[i].style.backgroundColor="blue";
+    y[i].style.color="red";
+    y[i].style.border="2px solid black";
+    y[i].style.margin="5px";
 }
-// TaskFun();
+}
+
+
+    
