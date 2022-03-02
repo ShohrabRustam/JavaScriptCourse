5a78ac1ec45102c6da46e60658101ad5bef8091e
Shohrab Rustam <mohdrustam001@gmail.com>
2022-03-02 14:43:31 +0530

    object prototype

---
+++ FirstDay/objectPrototype.js
@@ -0,0 +1,9 @@
+document.write("Welcome to the Prototype")
+var abc = {
+        Name: "Shohrab Rustam",
+        Subject : "JavaScript",
+        Address: "Bangalure",
+}
+
+document.write(abc);
+console.log(abc)
