1b7f5a588a4ac1e4ed294e6d48b39f6ce9e155cd
Shohrab Rustam <mohdrustam001@gmail.com>
2022-03-04 20:03:40 +0530

    arrow function

---
+++ FirstDay/arrowFunction.js
@@ -0,0 +1,13 @@
+// alert("Connected");
+function sum(a,b){
+    return a+b;
+}
+
+function func2(){
+    document.write("Hello JavaScript !");
+}
+func2();
+
+var Sub = (a,b)=>{
+    return a-b;
+}
