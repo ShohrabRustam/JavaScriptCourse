adb8456182fdac98059e1f6e279eb9d038aa59dd
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-26 10:38:30 +0530

    math opearator in js

---
+++ script/mathoperator.js
@@ -0,0 +1,5 @@
+let number1 = 100;
+let number2 = 200;
+
+console.log("Addition ", number1+number2);
+console.log("subtraction ", number1-number2);
---
+++ script/switchcase.js
@@ -0,0 +1,23 @@
+// switch(condition){
+
+// }
+
+let myAge = 20 ;
+
+switch(Age){
+    case 20:{
+        console.log("you are 20 ");
+        break;
+    }
+    case 21:{
+        console.log("you are 21 ");
+        break;
+    }
+    case 23:{
+        console.log("you are 23 ");
+        break;
+    }
+    default:
+        console.log("You ara neither 20 21 23  !!!" );
+        
+}
