e3ed7a95f8931b0a7536ce11860dca3e44ec9bc7
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-26 10:32:04 +0530

    if else condition

---
+++ script/conditionalstatement.js
@@ -0,0 +1,10 @@
+let myName = "Shohrab Rustam";
+
+if(myName=="Shohrab Rustam"){
+    console.log("Hello!! , Still the variable is Shohrab Rustam");
+}else if(myname=="Shohrab"){
+    console.log("The value of the variable is change !!");
+}
+else{
+    console.log("What the hell ??? ");
+}
