3b5f9a7c6187cc83e716fb46491447c81be9ce60
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-27 00:22:34 +0530

    key press

--- CarGame/carGame.js
+++ CarGame/carGame.js
@@ -10,6 +10,18 @@ const gameArea = document.querySelector('.gameArea')
 
 startScreen.addEventListener('click',start);
 
+document.addEventListener('keydown',pressOn);
+document.addEventListener('keyup',pressOff);
+ 
+function pressOn(e){
+    e.preventDefault();
+    console.log("On",e.key);
+}
+
+function pressOff(e){
+    console.log("Off",e.key);
+}
+
 function start(){
     alert("connected!!")
 }
