1eafc30592a343468e8f86222d26bec2cf560840
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-26 18:47:06 +0530

    target event listner

--- script/box.js
+++ script/box.js
@@ -1,22 +1,23 @@
-// const grandparent = document.querySelector('.grandParent');
-// const Parent = document.querySelector('.parent');
-// const Child = document.querySelector('.child');
-// // alert("Connected!!!")
-// grandparent.addEventListener('click',e=>{
-//     console.log(e.target); 
-// });
+const grandparent = document.querySelector('.grandParent');
+const Parent = document.querySelector('.parent');
+const Child = document.querySelector('.child');
+// alert("Connected!!!")
+grandparent.addEventListener('click',e=>{
+    if(e.target.matches('div'))
+   alert("HII!!!!!!"); 
+});
 
 
-const divs = document.querySelectorAll('div');
+// const divs = document.querySelectorAll('div');
 
-divs.forEach(div=>{
-    div.addEventListener("click",()=>{
-        alert("Hii !!!")
-    })
-})
+// divs.forEach(div=>{
+//     div.addEventListener("click",()=>{
+//         alert("Hii !!!")
+//     })
+// })
 
-const newDiv = document.createElement("div");
-newDiv.style.width="200px";
-newDiv.style.height="200px";
-newDiv.style.backgroundColor="purple";
-document.body.append(newDiv) ;
+// // const newDiv = document.createElement("div");
+// // newDiv.style.width="200px";
+// // newDiv.style.height="200px";
+// // newDiv.style.backgroundColor="purple";
+// // document.body.append(newDiv) ;
