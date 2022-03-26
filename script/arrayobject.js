6a6b9c80a099f2ce7ee4d04e22781df4c52578be
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-26 13:45:05 +0530

    find element from array of object

---
+++ script/arrayobject.js
@@ -0,0 +1,17 @@
+let students = [
+     {
+         name:"Shohrab",
+         age : 21
+     },
+     {
+         name: "Rustam",
+         age: 23
+     }
+
+    ];
+
+    const student= students.find(s=>{
+        return s.age===21;
+    });
+
+    console.log(student);
