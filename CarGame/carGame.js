941e192ba9e95d7042c2b0364a9f0fbf951d42c2
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-27 10:05:26 +0530

    add class in playgame

--- CarGame/carGame.js
+++ CarGame/carGame.js
@@ -7,13 +7,28 @@ const gameArea = document.querySelector('.gameArea')
 // console.log(score);
 // console.log(startScreen)
 // console.log(gameArea)
+
+//setup object to track key which key is pressed 
 let keys = {ArrowUp:false , ArrowDown:false , ArrowRight:false, ArrowLeft:false};
+
+let player = {};
 
 startScreen.addEventListener('click',start);
 
 document.addEventListener('keydown',pressOn);
 document.addEventListener('keyup',pressOff);
- 
+
+//animation 
+function playGame(){
+    startScreen.classList.remove('hide');
+    // gameArea.classList.add('hide')
+    console.log('inPlay');
+    if(player.strat){
+    window.requestAnimationFrame(playGame);
+    }
+}
+
+
 function pressOn(e){
     e.preventDefault();
     keys[e.key]=true;
@@ -21,10 +36,18 @@ function pressOn(e){
 }
 
 function pressOff(e){
+    // what key was press
+    e.preventDefault();
     keys[e.key]=false;
     console.log(keys);
 }
 
 function start(){
-    alert("connected!!")
+    // alert('connected');
+    player.start=true;
+    console.log('click')
+    window.requestAnimationFrame(playGame);
+    let car = document.createElement("div");
+    car.innerText = "Car";
+    gameArea.appendChild(car);
 }
